c435be59d60208a7a08ae020ad616efa50923480
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-01-08 16:31:44 +0100

    Initial import

---
+++ lfc_theme/static/js/lfctheme.js
@@ -0,0 +1,8 @@
+$(function() {
+    $("a.photo-image").lightBox();
+    // $("a.photo-image").prettyPhoto({
+    //  "animationSpeed" : "fast",
+    //  "opacity" : 0.5,
+    //  "theme" : "dark_rounded",
+    // });        
+});
---
+++ lfc_theme/templatetags/lfc_theme_tags.py
@@ -0,0 +1,43 @@
+from django.template import Library, Node, TemplateSyntaxError
+from django.utils.translation import ugettext as _
+
+# portlets imports
+import portlets.utils
+from portlets.models import Slot
+
+register = Library()
+
+class SlotsInformationNode(Node):
+    """
+    """
+    def render(self, context):
+        page = context.get("lfc_object")
+        if page:
+            page = page.get_specific_type()
+
+        for slot in Slot.objects.all():
+            context["Slot%s" % slot.name] = portlets.utils.has_portlets(slot, page)
+
+        if context["SlotLeft"] and context["SlotRight"]:
+            content_class = "span-13 append-1"
+        elif context["SlotLeft"]:
+            content_class = "span-18 last"
+        elif context["SlotRight"]:
+            content_class = "span-18 append-1"
+        else:
+            content_class = "span-24 last"
+
+        context["content_class"] = content_class
+        return ''
+
+def do_slots_information(parser, token):
+    """Calculates some context variables based on displayed slots.
+    """
+    bits = token.contents.split()
+    len_bits = len(bits)
+    if len_bits != 1:
+        raise TemplateSyntaxError(_('%s tag needs no argument') % bits[0])
+
+    return SlotsInformationNode()
+
+register.tag('slots_information', do_slots_information)
