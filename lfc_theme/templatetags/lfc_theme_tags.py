654b3c28a5bbe1b1eb33a856c27314e3c3433b33
Kai Diefenbach <kai.diefenbach@iqpp.de>
2010-07-04 15:04:00 +0200

    fix to issue 14 - cache keys are using CACHE_MIDDLEWARE_PREFIX now

--- lfc_theme/templatetags/lfc_theme_tags.py
+++ lfc_theme/templatetags/lfc_theme_tags.py
@@ -16,9 +16,11 @@ class SlotsInformationNode(Node):
         request = context.get("request")
 
         if obj:
-            cache_key = "slots-information-%s-%s-%s" % (obj.content_type, obj.id, request.user.id)
+            cache_key = "%s-slots-information-%s-%s-%s" % \
+                (settings.CACHE_MIDDLEWARE_KEY_PREFIX, obj.content_type, obj.id, request.user.id)
         else:
-            cache_key = "slots-information-portal-%s" % request.user.id
+            cache_key = "%s-slots-information-portal-%s" % \
+                (settings.CACHE_MIDDLEWARE_KEY_PREFIX, request.user.id)
 
         info = cache.get(cache_key)
         if info:
