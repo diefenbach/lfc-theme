240d0237f22f0567d08e1e4f8ea1082fe9dc8554
Kai Diefenbach <kai.diefenbach@iqpp.de>
2014-07-09 12:23:37 +0200

    Adds settings in order to change the width of the default slots.

--- lfc_theme/templatetags/lfc_theme_tags.py
+++ lfc_theme/templatetags/lfc_theme_tags.py
@@ -24,8 +24,12 @@ class SlotsInformationNode(Node):
                 (settings.CACHE_MIDDLEWARE_KEY_PREFIX, request.user.id)
 
         info = cache.get(cache_key)
+
+        info = False
         if info:
             context["content_class"] = info["content_class"]
+            context["left_class"] = info["left_class"]
+            context["right_class"] = info["right_class"]
             context["SlotLeft"] = info["SlotLeft"]
             context["SlotRight"] = info["SlotRight"]
             return ''
@@ -36,22 +40,33 @@ class SlotsInformationNode(Node):
         for slot in Slot.objects.all():
             context["Slot%s" % slot.name] = slot.has_portlets(obj)
 
+        max_width = 24
+        left_width = getattr(settings, "LFC_THEME_WIDTH_SLOT_LEFT", 5)
+        right_width = getattr(settings, "LFC_THEME_WIDTH_SLOT_RIGHT", 5)
+
         if context["SlotLeft"] and context["SlotRight"]:
-            content_class = "span-13 append-1"
+            content_class = "span-%s append-1" % (max_width - left_width - right_width - 1)
         elif context["SlotLeft"]:
-            content_class = "span-18 last"
+            content_class = "span-%s last" % (max_width - left_width)
         elif context["SlotRight"]:
-            content_class = "span-18 append-1"
+            content_class = "span-%s append-1" % (max_width - right_width - 1)
         else:
-            content_class = "span-24 last"
+            content_class = "span-%s last" % max_width
+
+        left_class = "span-%s" % left_width
+        right_class = "span-%s last" % right_width
 
         cache.set(cache_key, {
             "content_class": content_class,
+            "left_class": left_class,
+            "right_class": right_class,
             "SlotLeft": context["SlotLeft"],
             "SlotRight": context["SlotRight"],
         })
 
         context["content_class"] = content_class
+        context["left_class"] = left_class
+        context["right_class"] = right_class
         return ''
 
 
